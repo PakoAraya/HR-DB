b3dd9b4c70ce770c16b24ff0d7dc75349396a184
franarayah <franarayah@gmail.com>
2026-01-18 01:30:56 -0300

    Create the first and basics sql queries exercises

--- database/data/hr_queries.sql
+++ database/data/hr_queries.sql
@@ -36,26 +36,46 @@
 USE hr;
 
 -- =====================================================================
--- LAB 01: BASIC JOIN EXERCISE
--- Goal: List all employees with their department names and city.
+-- LAB 01: BASIC QUERIES EXERCISEs
+-- Goal: Learn best practices for declaring queries in databases
 -- =====================================================================
 
-/* Your first query could go here:
-SELECT 
-    e.first_name, 
-    e.last_name, 
-    d.department_name, 
-    l.city
-FROM employees e
-JOIN departments d ON e.department_id = d.department_id
-JOIN locations l ON d.location_id = l.location_id;
+/* 001
+Develop a query that list the employee's name, department code and de  
+hire date, sorting the results by departments and hire date with the most
+recent hire appearing first
 */
+SELECT  e.first_name, e.department_id, e.hire_date 
+FROM employees e 
+ORDER BY e.department_id, e.hire_date DESC ;
 
 
+/* 002
+ Develop a query that list employee's code, first and last names of the employees, and 
+ their respective managers, along with the titles of the employee and manager
+ boss, with job title and manager
+ */
+SELECT e1.employee_id ||' - '|| e1.first_name ||' '|| e1.last_name AS Employee, 
+e2.employee_id ||' - '|| e2.first_name ||' '|| e2.last_name AS Manager
+FROM employees e1, employees e2
+WHERE e1.manager_id = e2.employee_id ;
+
+-- Another Way
+SELECT e1.employee_id ||' - '|| e1.first_name ||' '|| e1.last_name AS Employee, 
+e2.employee_id ||' - '|| e2.first_name ||' '|| e2.last_name AS Manager
+FROM employees e1
+JOIN employees e2
+  ON e1.manager_id = e2.employee_id
 
 
-
-
+/* 003
+ Develop a query that list all countries by region. The data to be displayed is the region's code
+ and region's name, with the names of its countries.
+ */
+SELECT r.region_id, r.region_name, c.country_name 
+FROM regions r 
+JOIN countries c
+ON  r.region_id = c.region_id 
 
 
 
